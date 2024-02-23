8de5ad269529ed4ae8ddcdaee6ef095b69f7cfc0
Brittany Hagans <brittanyh88@gmail.com>
2024-02-23 13:41:02 -0500

    Yet another attempt to commit because the other 5 times didn't go through

---
+++ src/index.js
@@ -0,0 +1,44 @@
+// Menu data structure
+var menuLinks = [
+    { text: 'about', href: '/about' },
+    { text: 'catalog', href: '/catalog' },
+    { text: 'orders', href: '/orders' },
+    { text: 'account', href: '/account' },
+  ];
+
+// Part 1
+const mainEl = document.querySelector('main');
+// const mainEl = document.querySelector('main');
+mainEl.style.backgroundColor = 'var(--main-bg)';
+mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
+mainEl.classList.add('flex-ctr');
+
+//Part 2
+const topMenuEl = document.getElementById("top-menu");
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
+topMenuEl.classList.add("flex-around");
+
+//Part 3
+
+const nav = document.querySelector('nav')
+
+    for (var i = 0; i < menuLinks.length; i++) {
+        var link = menuLinks[i];
+        var menuItem = document.createElement('a');
+        menuItem.textContent = link.text;
+        menuItem.href = link.href;
+        nav.appendChild(menuItem);
+    }
+
+// item = document.createElement('a');
+//     item.textContent = link.text;
+//     item.href = link.href;
+//     menuContainer.appendChild(link);
+// // });
+// menuLinks.forEach(item => {
+//     item = document.createElement('a');
+//     item.textContent = link.text;
+//     item.href = link.href;
+//     menuContainer.appendChild(link);
+// });
